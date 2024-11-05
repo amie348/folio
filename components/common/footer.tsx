818d76dd3a1554651200b72064b7e25e9fde18d9
amie348 <ahmadyaqoob89@gmail.com>
2024-11-05 23:23:32 +0500

    resume changed

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
             target: "_blank",
             rel: "noreferrer",
           }}
-          href="/Ayush_Resume.pdf"
+          href="/Ahmad_Yaqoob_resume.pdf"
         ></Button>
         {/* <Button
           classes="ml-3"
